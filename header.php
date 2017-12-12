deaa03bd43ab5bb971fbf22ffa5ccca9024bc688
Fernando Britto <me@fernandobritto.com.br>
2017-12-12 21:56:51 -0200

    Initial Commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Semantic</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<header>
+		<h1>Header do Blog</h1>
+	</header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+<?php get_header('custom'); ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<p>
+				Semantic WP
+			</p>
+		</div>
+
+		<div class="col-md-6">
+			<?php get_sidebar(); ?>
+		</div>
+		
+	</div>
+</div>
+
+<?php get_footer(); ?>
+<?php get_footer('custom'); ?>
