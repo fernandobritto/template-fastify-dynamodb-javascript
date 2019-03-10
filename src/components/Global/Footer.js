ad2b4f584f3f05469ce9cf13bea47e0c42ac80c4
Fernando Britto <me@fernandobritto.com.br>
2019-03-10 13:34:06 -0300

    Initial commit

---
+++ src/components/Global/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './css/Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+          <h1>Footer</h1>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Global/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './css/Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+          <h1>Header</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
