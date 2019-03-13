8638a7048b24526a358a378ef60a02b542ec33ab
Fernando Britto <me@fernandobritto.com.br>
2019-03-12 21:52:27 -0300

    State

--- src/components/Global/Content.js
+++ src/components/Global/Content.js
@@ -7,11 +7,15 @@ class Content extends Component {
 		super();
 
 		this.state = {
-			count: 0
+			count: 0,
+			number1: 0,
+			number2: 0,
+			result: 0
 		};
 
 		this.handleCountClick = this.handleCountClick.bind(this);
-
+		this.handleResultClick = this.handleResultClick.bind(this);
+		this.handleInputChanged = this.handleInputChanged.bind(this);
 	}
 
 	componentDidMount(){
@@ -37,6 +41,24 @@ class Content extends Component {
 		}
 	}
 
+	handleResultClick(e){
+		this.setState({
+				result: this.state.number1 + this.state.number2
+			});
+	}
+
+	handleInputChanged(e){
+		if(e.target.id === 'number1'){
+			this.setState({
+				number1: Number(e.target.value)
+			});
+		}else if(e.target.id === 'number2'){
+			this.setState({
+				number2: Number(e.target.value)
+			});
+		}
+	}
+
 
 
   render() {
@@ -49,6 +71,18 @@ class Content extends Component {
           	<button id="subtr" onClick={this.handleCountClick}>-</button>
           	<button id="reset" onClick={this.handleCountClick}>Reset</button>
           </p>
+
+          <h2>Calculator</h2>
+          <p>
+          	<input id="number1" type="number" value={this.state.number1} onChange={this.handleInputChanged} />
+          	+
+          	<input id="number2" type="number" value={this.state.number2} onChange={this.handleInputChanged} />
+          	=
+          	<button id="result" onClick={this.handleResultClick}>Result</button>
+
+          	{this.state.result}
+          </p>
+
       </div>
     );
   }
