b6d00165f88c1dded53c8e5f6f583b4e5d7c529b
Fernando Britto <me@fernandobritto.com.br>
2019-04-07 07:05:47 -0300

    Initial commit

---
+++ view/menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Semantic Stock</title>
+</head>
+<body>
+
+	<?php 
+
+
+
+	require_once "dependencies.php"; 
+
+
+	?>
+	
+</body>
+</html>
