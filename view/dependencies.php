2ad62e511203299d80a465025d732ffea0bc7647
Fernando Britto <me@fernandobritto.com.br>
2019-04-11 09:13:57 -0300

    Initial commit

--- view/dependencies.php
+++ view/dependencies.php
@@ -1,11 +1,8 @@
 
-
-
 <link rel="stylesheet" type="text/css" href="../lib/alertifyjs/css/alertify.css">
 <link rel="stylesheet" type="text/css" href="../lib/alertifyjs/css/themes/default.css">
 <link rel="stylesheet" type="text/css" href="../lib/bootstrap/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="../lib/select2/css/select2.css">
-
 
 
 <script src="../lib/jquery.js"></script>
