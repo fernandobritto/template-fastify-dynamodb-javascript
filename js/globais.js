76f117ec3ced846ec520be3886a67b33dc450c91
Fernando Britto <me@fernandobritto.com.br>
2019-05-05 08:14:48 -0300

    Validação do formulário com JS

--- js/globais.js
+++ js/globais.js
@@ -1,11 +1,11 @@
 // função de validação de formulario
-function validarFormVazio() {
-	if(document.form.nome.value ==""){
-		alert("Por favor, preencha o campo nome.");
-		document.form.nome.focus();
-		return false;
-	}
+// function validarFormVazio() {
+// 	if(document.form.nome.value ==""){
+// 		alert("Por favor, preencha o campo nome.");
+// 		document.form.nome.focus();
+// 		return false;
+// 	}
 
-}
+// }
 
 
