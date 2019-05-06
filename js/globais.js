7cf7316744f200b8f54f6ea4236ae949b5123154
Fernando Britto <me@fernandobritto.com.br>
2019-05-06 09:45:23 -0300

    Initial commit

--- js/globais.js
+++ js/globais.js
@@ -1,11 +1,2 @@
 // função de validação de formulario
-// function validarFormVazio() {
-// 	if(document.form.nome.value ==""){
-// 		alert("Por favor, preencha o campo nome.");
-// 		document.form.nome.focus();
-// 		return false;
-// 	}
-
-// }
-
 
