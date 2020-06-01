631b57ce898bf27f9720d65e4ea04d38e7306876
Fernando Britto <fernandobritto.web@gmail.com>
2020-06-01 12:25:18 -0300

    Initial commit

---
+++ resources/views/content/content.blade.php
@@ -0,0 +1,54 @@
+@extends('main')
+@section('content')
+    <template v-if="menu==0">
+        <h1>Contenido del Menu 0</h1>
+    </template>
+
+    <template v-if="menu==1">
+        <example-component></example-component>
+    </template>
+
+    <template v-if="menu==2">
+        <h1>Contenido del Menu 2</h1>
+    </template>
+
+    <template v-if="menu==3">
+        <h1>Contenido del Menu 3</h1>
+    </template>
+
+    <template v-if="menu==4">
+        <h1>Contenido del Menu 4</h1>
+    </template>
+
+    <template v-if="menu==5">
+        <h1>Contenido del Menu 5</h1>
+    </template>
+
+    <template v-if="menu==6">
+        <h1>Contenido del Menu 6</h1>
+    </template>
+
+    <template v-if="menu==7">
+        <h1>Contenido del Menu 7</h1>
+    </template>
+
+    <template v-if="menu==8">
+        <h1>Contenido del Menu 8</h1>
+    </template>
+
+    <template v-if="menu==9">
+        <h1>Contenido del Menu 9</h1>
+    </template>
+
+    <template v-if="menu==10">
+        <h1>Contenido del Menu 10</h1>
+    </template>
+
+    <template v-if="menu==11">
+        <h1>Contenido del Menu 11</h1>
+    </template>
+
+    <template v-if="menu==12">
+        <h1>Contenido del Menu 12</h1>
+    </template>
+@endsection
